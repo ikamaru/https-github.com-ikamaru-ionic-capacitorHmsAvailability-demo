e913240f738741352c4996db7e815d25238b9f82
ikamaru <ikamaru.ikuane@gmail.com>
2021-03-29 12:06:31 +0100

    add HMS availability plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core';
+//import "capacitor-hms-availability";
+const { HmsAvailability } = Plugins;//import the HmsAvailability plugin
+
+import { ToastController } from '@ionic/angular'; //used Just in this demo to dislay result
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +13,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  
+  constructor(private toastController:ToastController) {}
 
+  isHmsAvailable(){
+    //check if HMS is available or not
+    HmsAvailability.isHmsAvailable().then(res=>{
+      if(res.isAvailable){
+        this.showToast("HMS is available");
+      }else{
+        this.showToast("HMS is not available");
+      }
+    })
+  }
+  isGmsAvailable(){
+    //check if GMS is available or not
+    HmsAvailability.isGmsAvailable().then(res=>{
+      if(res.isAvailable){
+        this.showToast("GMS is available");
+      }else{
+        this.showToast("GMS is not available");
+      }
+    })
+  }
+  private async showToast(msg){
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
